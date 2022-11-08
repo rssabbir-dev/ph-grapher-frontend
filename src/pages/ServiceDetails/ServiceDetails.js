cb58945aba46dae1c6f16afc27612e9f0a366cbf
rssabbir <sabbirhossainrahad@gmail.com>
2022-11-08 16:45:06 +0600

    added reviewsItem components and load data from server

--- src/pages/ServiceDetails/ServiceDetails.js
+++ src/pages/ServiceDetails/ServiceDetails.js
@@ -8,6 +8,7 @@ import ReviewSection from '../Services/ReviewSection';
 const ServiceDetails = () => {
 	const params = useParams();
 	const [service, setService] = useState({});
+	const [quantity, setQuantity] = useState(1);
 	const { _id, title, img, description, price } = service;
 	useEffect(() => {
 		const url = `${serverURL}/service/${params.id}`;
@@ -49,7 +50,7 @@ const ServiceDetails = () => {
 
 								<p className='text-lg font-bold'>
 									<span className='font-extrabold'>৳</span>
-									{price}
+									{price}/Per Hour
 								</p>
 							</div>
 
@@ -60,172 +61,41 @@ const ServiceDetails = () => {
 							</details>
 
 							<form className='mt-8'>
-								<fieldset>
-									<legend className='mb-1 text-sm font-medium'>
-										Color
-									</legend>
-
-									<div className='flow-root'>
-										<div className='-m-0.5 flex flex-wrap'>
-											<label
-												for='color_tt'
-												className='cursor-pointer p-0.5'
-											>
-												<input
-													type='radio'
-													name='color'
-													id='color_tt'
-													className='peer sr-only'
-												/>
-
-												<span className='group inline-block rounded-full border px-3 py-1 text-xs font-medium peer-checked:bg-black peer-checked:text-white'>
-													Texas Tea
-												</span>
-											</label>
-
-											<label
-												for='color_fr'
-												className='cursor-pointer p-0.5'
-											>
-												<input
-													type='radio'
-													name='color'
-													id='color_fr'
-													className='peer sr-only'
-												/>
-
-												<span className='group inline-block rounded-full border px-3 py-1 text-xs font-medium peer-checked:bg-black peer-checked:text-white'>
-													Fiesta Red
-												</span>
-											</label>
-
-											<label
-												for='color_cb'
-												className='cursor-pointer p-0.5'
-											>
-												<input
-													type='radio'
-													name='color'
-													id='color_cb'
-													className='peer sr-only'
-												/>
-
-												<span className='group inline-block rounded-full border px-3 py-1 text-xs font-medium peer-checked:bg-black peer-checked:text-white'>
-													Cobalt Blue
-												</span>
-											</label>
-										</div>
-									</div>
-								</fieldset>
-
 								<fieldset className='mt-4'>
 									<legend className='mb-1 text-sm font-medium'>
-										Size
+										Days
 									</legend>
 
 									<div className='flow-root'>
 										<div className='-m-0.5 flex flex-wrap'>
-											<label
-												for='size_xs'
-												className='cursor-pointer p-0.5'
-											>
-												<input
-													type='radio'
-													name='size'
-													id='size_xs'
-													className='peer sr-only'
-												/>
+											{[...Array(5).keys()].map(
+												(number) => (
+													<label
+														className='cursor-pointer p-0.5'
+														key={number}
+														onClick={() =>
+															setQuantity(
+																number + 1
+															)
+														}
+													>
+														<input className='peer sr-only' />
 
-												<span className='group inline-flex h-8 w-8 items-center justify-center rounded-full border text-xs font-medium peer-checked:bg-black peer-checked:text-white'>
-													XS
-												</span>
-											</label>
-
-											<label
-												for='size_s'
-												className='cursor-pointer p-0.5'
-											>
-												<input
-													type='radio'
-													name='size'
-													id='size_s'
-													className='peer sr-only'
-												/>
-
-												<span className='group inline-flex h-8 w-8 items-center justify-center rounded-full border text-xs font-medium peer-checked:bg-black peer-checked:text-white'>
-													S
-												</span>
-											</label>
-
-											<label
-												for='size_m'
-												className='cursor-pointer p-0.5'
-											>
-												<input
-													type='radio'
-													name='size'
-													id='size_m'
-													className='peer sr-only'
-												/>
-
-												<span className='group inline-flex h-8 w-8 items-center justify-center rounded-full border text-xs font-medium peer-checked:bg-black peer-checked:text-white'>
-													M
-												</span>
-											</label>
-
-											<label
-												for='size_l'
-												className='cursor-pointer p-0.5'
-											>
-												<input
-													type='radio'
-													name='size'
-													id='size_l'
-													className='peer sr-only'
-												/>
-
-												<span className='group inline-flex h-8 w-8 items-center justify-center rounded-full border text-xs font-medium peer-checked:bg-black peer-checked:text-white'>
-													L
-												</span>
-											</label>
-
-											<label
-												for='size_xl'
-												className='cursor-pointer p-0.5'
-											>
-												<input
-													type='radio'
-													name='size'
-													id='size_xl'
-													className='peer sr-only'
-												/>
-
-												<span className='group inline-flex h-8 w-8 items-center justify-center rounded-full border text-xs font-medium peer-checked:bg-black peer-checked:text-white'>
-													XL
-												</span>
-											</label>
+														<span
+															className={`group inline-flex h-8 w-8 items-center justify-center rounded-full border text-xs font-medium ${number+1 === quantity && 'bg-primary text-white'}`}
+														>
+															{number + 1}D
+														</span>
+													</label>
+												)
+											)}
 										</div>
 									</div>
 								</fieldset>
-
+								<div>
+									Total Cost: {price * (quantity*24)} Taka
+								</div>
 								<div className='mt-8 flex'>
-									<div>
-										<label
-											for='quantity'
-											className='sr-only'
-										>
-											Qty
-										</label>
-
-										<input
-											type='number'
-											id='quantity'
-											min='1'
-											value='1'
-											className='w-12 rounded border-gray-200 py-3 text-center text-xs [-moz-appearance:_textfield] [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none'
-										/>
-									</div>
-
 									<button
 										type='submit'
 										className='ml-3 block rounded px-5 py-3 text-xs btn btn-primary'
@@ -238,7 +108,7 @@ const ServiceDetails = () => {
 					</div>
 				</div>
 			</section>
-			<ReviewSection />
+			<ReviewSection service={service} />
 		</>
 	);
 };
--- src/pages/Services/ReviewSection.js
+++ src/pages/Services/ReviewSection.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider/AuthProvider';
+import { serverURL } from '../../routes/router';
+import ReviewItem from './ReviewItem';
 
-const ReviewSection = () => {
+const ReviewSection = ({ service }) => {
+	const { user } = useContext(AuthContext);
+	const [reviews,setReviews] = useState([])
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
+		reset
 	} = useForm();
 	const onSubmit = (data) => {
-		const { email, name } = data;
+		const { user_review } = data;
 		const modal = document.getElementById('my-modal');
+		const review = {
+			user_name: user.displayName,
+			user_email: user.email,
+			user_uid: user.uid,
+			user_photo: user.photoURL,
+			user_review: user_review,
+			user_rating: 4,
+			service_name: service.title,
+			service_id: service._id,
+			service_photo: service.img,
+			post_date: new Date(),
+		};
+		fetch(`${serverURL}/review`, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify(review),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.acknowledged) {
+					modal.checked = false;
+					reset()
+				}
+				console.log(data);
+			});
 	};
+	useEffect(() => {
+		fetch(`${serverURL}/reviews`)
+		.then(res => res.json())
+			.then(data => {
+			setReviews(data.reviews)
+		})
+	},[])
 	return (
 		<section>
 			{/* Review Modal */}
@@ -19,35 +60,54 @@ const ReviewSection = () => {
 			<input type='checkbox' id='my-modal' className='modal-toggle' />
 			<div className='modal'>
 				<form onSubmit={handleSubmit(onSubmit)} className='modal-box'>
-					<h3 className='font-bold text-lg'>
-						Congratulations random Internet user!
-					</h3>
-					<div className='form-control'>
-						<label className='label'>
-							<span className='label-text'>Name</span>
-						</label>
-						<input
-							type='text'
-							placeholder='name'
-							className='input input-bordered'
-							{...register('name', {
-								required: true,
-							})}
-						/>
-					</div>
-					<label className='label'>
-						{errors.name && (
-							<span className='label-text-alt'>
-								This field is required
-							</span>
-						)}
-					</label>
-					<div className='modal-action'>
-						<label htmlFor='my-modal' className='btn'>
-							Close
-						</label>
-						<button className='btn btn-primary'>Submit</button>
-					</div>
+					{user?.uid ? (
+						<div>
+							<h3 className='font-bold text-lg'>Add a review</h3>
+
+							<div className='form-control'>
+								<textarea
+									placeholder='Describe you feedback'
+									className='textarea textarea-bordered mt-3'
+									{...register('user_review', {
+										required: true,
+									})}
+								/>
+							</div>
+							<label className='label'>
+								{errors.user_review && (
+									<span className='label-text-alt'>
+										This field is required
+									</span>
+								)}
+							</label>
+							<div className='modal-action'>
+								<label htmlFor='my-modal' className='btn'>
+									Close
+								</label>
+								<button className='btn btn-primary'>
+									Submit
+								</button>
+							</div>
+						</div>
+					) : (
+						<div>
+							<div className='flex flex-col gap-5 justify-center items-center'>
+								<h3 className='text-2xl'>
+									Please login to add a review
+								</h3>
+								<Link className='btn btn-primary' to='/login'>
+									Login Now
+								</Link>
+							</div>
+							<div>
+								<div className='modal-action'>
+									<label htmlFor='my-modal' className='btn'>
+										Close
+									</label>
+								</div>
+							</div>
+						</div>
+					)}
 				</form>
 			</div>
 			{/* Review Modal */}
@@ -123,265 +183,9 @@ const ReviewSection = () => {
 				</div>
 
 				<div className='mt-8 grid grid-cols-1 gap-x-16 gap-y-12 lg:grid-cols-2'>
-					<blockquote>
-						<header className='sm:flex sm:items-center'>
-							<div className='-ml-1 flex'>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-gray-200'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-							</div>
-
-							<p className='mt-2 font-medium sm:ml-4 sm:mt-0'>
-								The best thing money can buy!
-							</p>
-						</header>
-
-						<p className='mt-2 text-gray-700'>
-							Lorem ipsum dolor sit amet, consectetur adipisicing
-							elit. Ullam possimus fuga dolor rerum dicta, ipsum
-							laboriosam est totam iusto alias incidunt cum
-							tempore aliquid aliquam error quisquam ipsam
-							asperiores! Iste?
-						</p>
-
-						<footer className='mt-4'>
-							<p className='text-xs text-gray-500'>
-								John Doe - 12th January, 2024
-							</p>
-						</footer>
-					</blockquote>
-
-					<blockquote>
-						<header className='sm:flex sm:items-center'>
-							<div className='-ml-1 flex'>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-gray-200'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-							</div>
-
-							<p className='mt-2 font-medium sm:ml-4 sm:mt-0'>
-								The best thing money can buy!
-							</p>
-						</header>
-
-						<p className='mt-2 text-gray-700'>
-							Lorem ipsum dolor sit amet, consectetur adipisicing
-							elit. Ullam possimus fuga dolor rerum dicta, ipsum
-							laboriosam est totam iusto alias incidunt cum
-							tempore aliquid aliquam error quisquam ipsam
-							asperiores! Iste?
-						</p>
-
-						<footer className='mt-4'>
-							<p className='text-xs text-gray-500'>
-								John Doe - 12th January, 2024
-							</p>
-						</footer>
-					</blockquote>
-
-					<blockquote>
-						<header className='sm:flex sm:items-center'>
-							<div className='-ml-1 flex'>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-gray-200'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-							</div>
-
-							<p className='mt-2 font-medium sm:ml-4 sm:mt-0'>
-								The best thing money can buy!
-							</p>
-						</header>
-
-						<p className='mt-2 text-gray-700'>
-							Lorem ipsum dolor sit amet, consectetur adipisicing
-							elit. Ullam possimus fuga dolor rerum dicta, ipsum
-							laboriosam est totam iusto alias incidunt cum
-							tempore aliquid aliquam error quisquam ipsam
-							asperiores! Iste?
-						</p>
-
-						<footer className='mt-4'>
-							<p className='text-xs text-gray-500'>
-								John Doe - 12th January, 2024
-							</p>
-						</footer>
-					</blockquote>
-
-					<blockquote>
-						<header className='sm:flex sm:items-center'>
-							<div className='-ml-1 flex'>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-yellow-400'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									className='h-5 w-5 text-gray-200'
-									viewBox='0 0 20 20'
-									fill='currentColor'
-								>
-									<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-								</svg>
-							</div>
-
-							<p className='mt-2 font-medium sm:ml-4 sm:mt-0'>
-								The best thing money can buy!
-							</p>
-						</header>
-
-						<p className='mt-2 text-gray-700'>
-							Lorem ipsum dolor sit amet, consectetur adipisicing
-							elit. Ullam possimus fuga dolor rerum dicta, ipsum
-							laboriosam est totam iusto alias incidunt cum
-							tempore aliquid aliquam error quisquam ipsam
-							asperiores! Iste?
-						</p>
-
-						<footer className='mt-4'>
-							<p className='text-xs text-gray-500'>
-								John Doe - 12th January, 2024
-							</p>
-						</footer>
-					</blockquote>
+					{
+						reviews.map(review => <ReviewItem key={review._id} review={review}/>)
+					}
 				</div>
 			</div>
 		</section>
